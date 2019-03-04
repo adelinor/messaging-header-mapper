46391db45ff55a7fc3963b7c0847e92c13dc602d
adelinor <adelinor@gmail.com>
2019-03-04 08:30:47 +0000

    Add comments

--- src/main/java/com/github/adelinor/messaging/mapper/MapHeaderMapper.java
+++ src/main/java/com/github/adelinor/messaging/mapper/MapHeaderMapper.java
@@ -11,8 +11,20 @@ import java.util.Map;
  */
 public interface MapHeaderMapper<T> {
 
+	/**
+	 * Maps values from message headers to java bean object.
+	 * 
+	 * @param headers Headers as a map
+	 * @param target Target java bean object to populate
+	 */
 	void fromHeaders(Map<String, Object> headers, T target);
 	
+	/**
+	 * Maps values from java bean object to message headers.
+	 * 
+	 * @param source Source of headers values as a java bean object
+	 * @param headers Headers as a map to populate
+	 */
 	void toHeaders(T source, Map<String, Object> headers);
 
 }
